d7f1f95c5066c6ef2012a0dc25e630eec2af5dd0
Zsanett Ferenczi <ferenczizsani@gmail.com>
2023-03-13 13:25:40 +0100

    Minor changes, css

--- src/components/content/Packages/Package/Package.js
+++ src/components/content/Packages/Package/Package.js
@@ -5,12 +5,13 @@ import classes from './Package.module.css';
 const packageComp = (props) => {
 	let contentList = Object.keys(props.content).map((elem) => {
 		return (
-			<li
-				key={elem}
-				className={props.content[elem] ? classes.Good : classes.Bad}
-			>
-				{props.content[elem] ? <FaCheckCircle /> : <FaMinusCircle />}
-				{elem}
+			<li key={elem} className={!props.content[elem] ? classes.BadText : undefined}>
+				{props.content[elem] ? (
+					<FaCheckCircle className={classes.Good} />
+				) : (
+					<FaMinusCircle className={classes.Bad} />
+				)}
+				<span>{elem}</span>
 			</li>
 		);
 	});
