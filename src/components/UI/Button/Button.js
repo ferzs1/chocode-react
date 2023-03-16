a657afdf0b854dfa4b6b1ed80b0d064d94b26516
Zsanett Ferenczi <ferenczizsani@gmail.com>
2023-03-16 14:54:52 +0100

    Location when button is clicked

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import classes from './Button.module.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Button = (props) => {
 	const navigate = useNavigate();
+  	const location = useLocation();
 
 	const scrollToSection = async (sec) => {
-		await navigate('/');
-		document
-			.getElementById(sec.substring(1))
-			.scrollIntoView({ behavior: 'smooth' });
+		if (location.pathname !== '/') {
+			await navigate('/');
+		}
+		document.getElementById(sec.substring(1)).scrollIntoView({ behavior: 'smooth' });
 	};
 
-	let cls = [classes.Button, classes[props.type]].join(' ');
+	let cls = [classes.Link];
+	if (props.btnStyle === 'button') {
+		cls = [classes.Button, classes[props.type]].join(' ');
+	}
 
 	let elem = '';
 	if (props.href.startsWith('#')) {
 		// scroll smooth
 		elem = (
-			<span onClick={() => {scrollToSection(props.href); }} className={cls}>
+			<span onClick={() => {scrollToSection(props.href);}} className={cls}>
 				{props.children}
 			</span>
 		);
@@ -32,12 +36,6 @@ const Button = (props) => {
 	}
 
     return elem;
-	return (
-		<a href={props.href} className={cls}>
-			{props.children}
-		</a>
-
-	);
 };
 
 export default Button;
