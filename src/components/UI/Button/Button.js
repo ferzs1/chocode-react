a0fb85ecba588b793269821bfdcbcda6962adf90
Zsanett Ferenczi <ferenczizsani@gmail.com>
2023-03-16 14:19:58 +0100

    Scroll feat to button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import classes from './Button.module.css';
+import { useNavigate } from 'react-router-dom';
 
-const button = (props) => {
-    let cls = [classes.Button, classes[props.type]].join(' ')
-    return (
-        <button
-            onClick={props.onClick}
-            className={cls}>
-            {props.children}
-        </button>
-    );
-}
+const Button = (props) => {
+	const navigate = useNavigate();
 
-export default button;
+	const scrollToSection = async (sec) => {
+		await navigate('/');
+		document
+			.getElementById(sec.substring(1))
+			.scrollIntoView({ behavior: 'smooth' });
+	};
+
+	let cls = [classes.Button, classes[props.type]].join(' ');
+
+	let elem = '';
+	if (props.href.startsWith('#')) {
+		// scroll smooth
+		elem = (
+			<span onClick={() => {scrollToSection(props.href); }} className={cls}>
+				{props.children}
+			</span>
+		);
+	} else {
+		// href
+		elem = (
+			<a href={props.href} className={cls}>
+				{props.children}
+			</a>
+		);
+	}
+
+    return elem;
+	return (
+		<a href={props.href} className={cls}>
+			{props.children}
+		</a>
+
+	);
+};
+
+export default Button;
